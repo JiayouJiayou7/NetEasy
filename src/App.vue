57110ab3115230ae65a898d28df274e0c267afae
jiayoujiayou7 <Ord1nary1996@163.com>
2020-05-07 11:12:35 +0800

    完成底部footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="App">
     <router-view></router-view>
+    <FooterWrap></FooterWrap>
   </div>
 </template>
 
 <script>
+import FooterWrap from './components/FooterWrap/FooterWrap'
   export default {
-    name: 'App'
+    name: 'App',
+    components:{
+      FooterWrap
+    }
   }
 </script>
 
---
+++ src/components/FooterWrap/FooterWrap.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="footerContainer">
+      <div class="footerItem" @click="toggleNav('/home')" :class="{active:$route.path == '/home'}">
+        <i class="iconfont icon-shouye"></i>
+       <span>首页</span>
+      </div> 
+      <div class="footerItem" @click="toggleNav('/category')" :class="{active:$route.path == '/category'}">
+         <i class="iconfont icon-fenlei"></i>
+       <span>分类</span>
+      </div>  
+      <div class="footerItem" @click="toggleNav('/worthbuying')" :class="{active:$route.path == '/worthbuying'}">
+        <i class="iconfont icon-iconfontzhizuobiaozhun023148"></i>
+       <span>值得买</span>
+      </div>  
+      <div class="footerItem" @click="toggleNav('/cartlist')" :class="{active:$route.path == '/cartlist'}">
+        <i class="iconfont icon-gouwuche"></i>
+       <span>购物车</span>
+      </div>  
+      <div class="footerItem" @click="toggleNav('/personal')" :class="{active:$route.path == '/personal'}">
+        <i class="iconfont icon-wodejuhuasuan"></i>
+       <span>购物车</span>
+      </div>  
+  </div>
+</template>
+
+<script>
+  export default {
+    methods:{
+      toggleNav(path){
+        // replace跳转到指定的url
+        path !== this.$router.replace(path)
+      }
+    }
+    
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .footerContainer
+    display flex
+    width 100%
+    height 98px
+    .footerItem
+      display flex
+      flex-direction column
+      text-align center
+      height 100%
+      width 20%
+      &.active
+        color #f40
+      .iconfont
+        font-size 36px
+        line-height 60px
+      
+  
+
+</style>
