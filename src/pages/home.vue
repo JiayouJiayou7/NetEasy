adb23ffe31501ddaec04db8e0115ed6044560691
jiayoujiayou7 <Ord1nary1996@163.com>
2020-05-08 20:50:37 +0800

    热销榜完成

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -12,6 +12,7 @@
         <button class="login">登录</button>
       </div>
       <!-- 头部导航 -->
+     
       <div class="nav-list" ref="navlist">
         <ul>
           <li v-for="(navItem,index) in indexCateModule" 
@@ -23,8 +24,9 @@
         </ul>
       </div>
     </div>
-    <!-- 轮播图 -->
-     <div class="Swiper-container">
+     <div class="content-wrap">
+       <!-- 轮播图 -->
+     <div class="swiper-container">
         <div class="swiper-wrapper">
           <div class="swiper-slide" v-for="(focusItem, index) in focusList" :key="index">
             <img :src="focusItem.picUrl"/>
@@ -90,6 +92,42 @@
          </div>
         </div>
       </div>
+      <!-- 热销榜 -->
+      <div class="category-hotSell-module">
+      <div class="hotSell-module-title"><span>类目热销榜</span></div>
+      <div class="hotSell-module-content">
+        <div class="content-top">
+          <div class="content-top-left">
+            <div class="text">
+              <span>热销榜</span>
+            </div>
+            <div class="img-wrap">
+              <img src="https://yanxuan-item.nosdn.127.net/db4a9cbd2bdfed303e2536db8b40fe33.png?quality=75&type=webp&imageView&thumbnail=200x200" alt="">
+            </div>
+          </div>
+          <div class="content-top-right">
+             <div class="text">
+              <span>好评榜</span>
+            </div>
+            <div class="img-wrap">
+              <img src="https://yanxuan-item.nosdn.127.net/bb9025c24057dfb89403055ac5b9f85c.png?quality=75&type=webp&imageView&thumbnail=200x200" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="content-bottom">
+          <ul>
+            <li v-for="(categoryItem, index) in categoryList" :key="index">
+              <div class="title"><span>{{categoryItem.categoryName}}</span></div>
+              <div class="img-wrap">
+               <img :src="categoryItem.showPicUrl" alt="">
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+     </div>
+    
   </div>
 </template>
 
@@ -128,18 +166,19 @@ import {mapState} from "vuex";
       focusList: state => state.focusList,
       policyDescList: state => state.policyDescList,
       kingKongList: state => state.kingKongList,
+      categoryList: state => state.categoryList,
       })
      },
     watch: {
     indexCateModule(){
         this.$nextTick(() => {
         //创建Swiper实例对象
-        new Swiper(".Swiper-container", {
+        new Swiper(".swiper-container", {
           loop: true,
           //分页器
           autoplay:true,
           pagination: {
-            el: '.swiper-pagination'
+            el: '.swiper-pagination',
           },
         });
       });
@@ -230,9 +269,10 @@ import {mapState} from "vuex";
                 bottom 0
                 width 100%
                 height 4px
-                background-color $mainColor
+                background-color red
+
                 // 轮播图
-    .Swiper-container
+    .swiper-container
       width 100%
       height 370px
       margin-top 150px
@@ -253,7 +293,7 @@ import {mapState} from "vuex";
           .swiper-pagination-bullet-active
             width 40px
             height 5px
-            background-color #fff
+            background-color red
     .policy-service
       width 100%
       height 72px
@@ -404,7 +444,94 @@ import {mapState} from "vuex";
               height 200px  
         .module-2
           background-color #FFECC2
-
+    .category-hotSell-module
+      width 100%
+      padding 0 30px 20px
+      box-sizing border-box
+      background-color #fff
+      .hotSell-module-title
+        width 100%
+        height 100px
+        line-height 100px
+        font-size 32px
+    
+      .hotSell-module-content
+        width 100%
+        .content-top
+          display flex
+          width 100%
+          height 200px
+          >div
+            width 50%
+            height 100%
+            border-radius 5px
+          .content-top-left, .content-top-right
+            display flex
+            width 50%
+            background-color #f9f3e4
+            padding-left 20px
+            line-height 150px
+            margin 0 10px 10px 0
+            >div
+              width 50%
+              height 100%
+            .text
+              position relative
+              font-size 28px
+              &::after
+                content ''
+                display block
+                position absolute
+                left 0
+                top 50%
+                margin-top 10px
+                width 48px
+                height 4px
+                background-color #666
+            .img-wrap
+              width 100%
+              height 100%
+              img 
+                width 100%
+                height 100%
+          .content-top-right
+            background-color #ebeff6
+            margin-right 0
+        .content-bottom
+          margin-top 10px
+          width 100%
+          ul
+           width 100%
+           overflow hidden
+           box-sizing border-box
+           li
+            position relative
+            width 165px
+            height 180px
+            float left
+            border-radius 5px
+            background-color #f5f5f5
+            margin 0 10px 10px 0
+            &:nth-child(4), &:nth-child(8)
+              margin-right 0
+            .title
+              width 100%
+              height 36px
+              font-size 24px
+              text-align center
+              line-height 36px
+              padding-top 10px
+            .img-wrap 
+              position absolute
+              left 50%
+              transform translateX(-50%)
+              top 50px
+              width 120px
+              height 120px
+              text-align center
+              img 
+                width 100%
+                height 100%
 
 
 </style>
